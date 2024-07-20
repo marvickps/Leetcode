156ffa4de3312fa5b18407ef8f7d0a2b0db04272
Meraj Alam <73574203+marvickps@users.noreply.github.com>
2024-07-21 00:28:24 +0530

    Time: 7 ms (98.44%) | Memory: 11.6 MB (60.29%) - LeetSync

--- 66-plus-one/plus-one.py
+++ 66-plus-one/plus-one.py
@@ -4,17 +4,22 @@ class Solution(object):
         :type digits: List[int]
         :rtype: List[int]
         """
-        strTemp=""
-        final=[]
-        for i in range(len(digits)):
-            strTemp+=str(digits[i])
+        #6999
+        n = len(digits)
 
-        num=int(strTemp)
-        num+=1
-        strTemp=str(num)
-        for i in range(0,len(str(num))):
-            final.append(int(strTemp[i]))
-        
-        return final
+        if 0 <= digits[n - 1] <= 8:
+            digits[n - 1] = digits[n - 1] + 1
+        else:
+            for i in range(n - 1, -1, -1):
+                if digits[i] == 9:
+                    digits[i] = 0
+                    if i == 0:
+                        digits.insert(0, 1)
+                else:
+                    digits[i] = digits[i] + 1
+                    break
 
+        return digits
+
+                
         
