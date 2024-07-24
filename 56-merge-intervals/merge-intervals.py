3ad4bb3aa310ac6366cb3f41a73bdc90a59a1e6f
Meraj Alam <73574203+marvickps@users.noreply.github.com>
2024-07-24 23:24:51 +0530

    Time: 98 ms (72.00%) | Memory: 16.7 MB (43.64%) - LeetSync

--- 56-merge-intervals/merge-intervals.py
+++ 56-merge-intervals/merge-intervals.py
@@ -4,19 +4,15 @@ class Solution(object):
         :type intervals: List[List[int]]
         :rtype: List[List[int]]
         """
-        i=1
-        
-        while i < len(intervals):
-            intervals.sort()
-            if intervals[i-1][1]>=intervals[i][1] or intervals[i-1][1] >= intervals[i][0]:
-                intervals.insert(0,[min(intervals[i-1][0],intervals[i][0]),max(intervals[i-1][1],intervals[i][1])])
-                intervals.remove(intervals[i])
-                intervals.remove(intervals[i])
-            
-            else:i += 1
-            
+        intervals.sort()
+        merged = []
 
+        for interval in intervals:
+            if not merged or merged[-1][1] < interval[0]:
+                merged.append(interval)
+            else:
+                merged[-1][1] = max(merged[-1][1], interval[1])
 
-        return intervals
+        return merged
         
         
