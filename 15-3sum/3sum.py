8dd371956ac59079052aa4e1e4e506caea4c140c
Meraj Alam <alamm1940@gmail.com>
2026-08-17 13:18:01 +0530

    Time: 649 ms (44.82%) | Memory: 22.4 MB (17.73%) - LeetSync

--- 15-3sum/3sum.py
+++ 15-3sum/3sum.py
@@ -1,23 +1,33 @@
-class Solution(object):
-    def threeSum(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: List[List[int]]
-        """
-        nums.sort()
-        dictMap={}
-        retList = []
-        temp=0
-        for i in range(len(nums)):
-            dictMap={}
-            for j in range(i+1,len(nums)):
-                firstNum = (-nums[i])-nums[j]
-                if firstNum in dictMap and ([nums[i],nums[j],firstNum]) not in retList:
-                    retList.append([nums[i],nums[j],firstNum])
-                    dictMap.pop(firstNum)
-                else:
-                    dictMap[nums[j]] = firstNum
-
-        return retList
+class Solution:
+    def threeSum(self, nums: list[int]) -> list[list[int]]:
+        #[-4,-1,-1,0,1,2]
+             #l        r -> [-1,-1,2]
+                #l     r -> 3>2
+                #l    r -> 
+        nums = sorted(nums)
+        le =len(nums)
+        result = []
+        for i in range(le):
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
+            l = i+1
+            r = le-1
             
-        
+            while l<r:
+
+                current = nums[l] + nums[r]
+                if current >  -nums[i]:
+                    r -= 1
+                elif current <  -nums[i]:
+                    l += 1
+                else:
+                    result.append([nums[l], nums[r], nums[i]])
+                    l +=1
+                    r -=1
+                    while l < r and nums[l] == nums[l - 1]:
+                        l += 1
+
+                    while l < r and nums[r] == nums[r + 1]:
+                        r -= 1
+
+        return result
