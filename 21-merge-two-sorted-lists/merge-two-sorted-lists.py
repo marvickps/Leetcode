3f87ddfe84c38e0d71e928cfe8860940a7d78df4
Meraj Alam <alamm1940@gmail.com>
2026-09-01 17:33:02 +0530

    Time: 0 ms (100.00%) | Memory: 19.2 MB (94.62%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.py
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.py
@@ -1,38 +1,25 @@
 # Definition for singly-linked list.
-# class ListNode(object):
+# class ListNode:
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-class Solution(object):
-    def mergeTwoLists(self, list1, list2):
-        """
-        :type list1: Optional[ListNode]
-        :type list2: Optional[ListNode]
-        :rtype: Optional[ListNode]
-        """
-
-        temp = ListNode()
-        final = temp
-
-
-        while list1 and list2:
-            if list1.val < list2.val:
-                final.next = list1
-                list1 = list1.next
-
-            else:
-                final.next  = list2
-                list2 = list2.next
-            final = final.next
-
-        if list1:
-            final.next = list1
-        if list2:
-            final.next =list2
-
-
-        return temp.next
-
-
-
+class Solution:
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
         
+        result = ListNode()
+        firstPointer = result
+        
+        while list1 and list2:
+            if list1.val<list2.val: 
+                result.next = list1 #result -> 1-1-2-4
+                list1 = list1.next #list1 -> 2-4
+            else:
+                result.next = list2 #result -> none-1-3-4
+                list2 = list2.next #3-4
+            
+            result = result.next #1-3-4 #
+        if list1:
+            result.next = list1
+        if list2:
+            result.next = list2             
+        return firstPointer.next
